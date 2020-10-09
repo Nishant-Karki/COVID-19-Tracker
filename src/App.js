b03dc61782c9b539a7b6dcb61df46f6ba0ec19a4
Nishant <karkinishant14@gmail.com>
2020-10-09 12:32:50 +0545

    covid-tracker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+import Chart from "./components/Chart/Chart";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+import Cards from "./components/Card/Card";
+import { ThemeProvider, MuiThemeProvider } from "@material-ui/core/styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import coronaImage from "./images/image.png";
+
+export class App extends Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={this.state.country} />
+        <small>Reference:JavaScript Mastery</small>
+      </div>
+    );
+  }
 }
 
 export default App;
